2ef7b0809a2396f85e1bce2bfafbc9d5812cd622
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-08-18 15:59:39 +0900

    Divide pages & load them as components, create navbar desktop version

---
+++ js/index.js
@@ -0,0 +1,4 @@
+import { loadComponent } from "./utilities";
+
+loadComponent("navigation", "../html/navigation.html");
+loadComponent("footer", "../html/footer.html");
---
+++ js/utilities.js
@@ -0,0 +1,8 @@
+async function loadComponent(id, file) {
+  const response = await fetch(file);
+  const text = await response.text();
+
+  document.getElementById(id).innerHTML = text;
+}
+
+export { loadComponent };
