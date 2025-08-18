78c0f37e993d78d023e81d7bd6d331a62c5ef729
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-08-18 17:17:26 +0900

    Add js code to control mobile navigation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 import { loadComponent } from "./utilities";
 
-loadComponent("navigation", "../html/navigation.html");
-loadComponent("footer", "../html/footer.html");
+loadComponent("navigation", "../html/navigation.html").then(() => {
+  const toggleNavigation = document.querySelector(
+    "[aria-controls='primary-navigation']",
+  );
+
+  toggleNavigation.addEventListener("click", () => {
+    const navigationOpened = toggleNavigation.getAttribute("aria-expanded");
+    if (navigationOpened === "false") {
+      toggleNavigation.setAttribute("aria-expanded", "true");
+    } else {
+      toggleNavigation.setAttribute("aria-expanded", "false");
+    }
+  });
+});
+
+// loadComponent("footer", "../html/footer.html");
