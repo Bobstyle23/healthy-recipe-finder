19a847493dd07d6c5837a94a0fe75262dee261c3
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-08-19 14:33:26 +0900

    Add attributes to indicate active page

--- js/index.js
+++ js/index.js
@@ -4,6 +4,19 @@ loadComponent("navigation", "../html/navigation.html").then(() => {
   const toggleNavigation = document.querySelector(
     "[aria-controls='primary-navigation']",
   );
+  const navigationMenu = document.querySelectorAll(".navigation__list li");
+
+  const menuItems = [...navigationMenu].slice(0, 3);
+
+  menuItems.forEach((menu) => {
+    menu.addEventListener("click", function () {
+      const isActive = this.hasAttribute("data-active");
+      if (isActive) return;
+
+      menuItems.forEach((element) => element.removeAttribute("data-active"));
+      menu.setAttribute("data-active", "true");
+    });
+  });
 
   toggleNavigation.addEventListener("click", () => {
     const navigationOpened = toggleNavigation.getAttribute("aria-expanded");
