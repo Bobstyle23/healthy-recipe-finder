dc0d4a7f6af0984d3c849abbb240c8cb74ebd520
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-08-22 17:11:13 +0900

    Create recipes container & add recipe contents & style (mobile & small tablets)

--- js/index.js
+++ js/index.js
@@ -1,25 +1,15 @@
 import data from "../data.json";
 
 const recipesSelectOptions = document.querySelectorAll(".recipes__select");
-
-[...recipesSelectOptions].forEach((select) => {
-  select.addEventListener("click", (e) => {
-    const selectOpened = select.getAttribute("aria-expanded");
-    if (selectOpened === "false") {
-      select.setAttribute("aria-expanded", "true");
-    } else {
-      select.setAttribute("aria-expanded", "false");
-    }
-  });
-});
-
 const toggleNavigation = document.querySelector(
   "[aria-controls='primary-navigation']",
 );
 const navigationMenu = document.querySelectorAll(".navigation__list li");
+const recipesContainer = document.querySelector(".recipes__container");
 
 const menuItems = [...navigationMenu].slice(0, 3);
 
+// NOTE: Resize observer to add resizing class to body
 const resizeObserver = new ResizeObserver(() => {
   document.body.classList.add("resizing");
 
@@ -30,6 +20,7 @@ const resizeObserver = new ResizeObserver(() => {
 
 resizeObserver.observe(document.body);
 
+// NOTE: Page navigating
 menuItems.forEach((menu, index) => {
   menu.addEventListener("click", function (event) {
     event.preventDefault();
@@ -41,6 +32,7 @@ menuItems.forEach((menu, index) => {
   });
 });
 
+//NOTE: Active menu highlighting
 const savedIndex = localStorage.getItem("activeMenu");
 if (savedIndex !== null) {
   menuItems.forEach((el) => el.removeAttribute("data-active"));
@@ -51,6 +43,7 @@ if (window.location.href.includes("index") && savedIndex !== 0) {
   menuItems[0].setAttribute("data-active", "true");
 }
 
+//NOTE: Mobile navigation toggler
 toggleNavigation.addEventListener("click", () => {
   const navigationOpened = toggleNavigation.getAttribute("aria-expanded");
   if (navigationOpened === "false") {
@@ -59,3 +52,55 @@ toggleNavigation.addEventListener("click", () => {
     toggleNavigation.setAttribute("aria-expanded", "false");
   }
 });
+
+//NOTE: Recipes cook & prep time options
+[...recipesSelectOptions].forEach((select) => {
+  select.addEventListener("click", (e) => {
+    const selectOpened = select.getAttribute("aria-expanded");
+    if (selectOpened === "false") {
+      select.setAttribute("aria-expanded", "true");
+    } else {
+      select.setAttribute("aria-expanded", "false");
+    }
+  });
+});
+
+const recipeDetail = data.map((recipe) => {
+  return `
+    <article class="recipe">
+      <picture class="recipe__image">
+          <source
+            srcset=${recipe.image.large}
+            type="image/webp"
+            media="(min-width: 48em)"
+          />
+          <img
+            loading="lazy"
+            src=${recipe.image.small}
+            alt="A Smiling family showing off by holding fresh tomatoes, greens, paprika, and lemon in their modern kitchen"
+          />
+      </picture>
+      <div class="recipe__info">
+          <h3 class="recipe__title">${recipe.title}</h3>
+          <p class="recipe__desc">${recipe.overview}</p>
+      </div>
+      <div class="recipe__preparation">
+        <div>
+         <img src="../assets/images/icon-servings.svg" />
+         <p>Servings: ${recipe.servings}</p>
+       </div>
+
+       <div>
+         <img src="../assets/images/icon-prep-time.svg" />
+         <p>Prep: ${recipe.prepMinutes}  ${recipe.prepMinutes > 1 ? "mins" : "min"}</p>
+       </div>
+      <div>
+         <img src="../assets/images/icon-cook-time.svg" />
+         <p>Cook: ${recipe.cookMinutes} ${recipe.cookMinutes > 1 ? "mins" : "min"} </p>
+       </div>
+       </div>
+      <a href=${recipe.slug} class="recipe__link button">View Recipe</a>
+    </article>
+`;
+});
+recipesContainer.innerHTML = recipeDetail.join("");
