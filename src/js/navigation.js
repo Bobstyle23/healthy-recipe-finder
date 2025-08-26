8d9a531f0818b2a37a4e512dbc9731e31e3e6b8c
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-08-26 16:35:30 +0900

    Update navigation active state

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -6,6 +6,31 @@ const navigationMenu = document.querySelectorAll(".navigation__list li");
 
 const menuItems = [...navigationMenu].slice(0, 3);
 
+function extractLocation(location) {
+  return location.split("/").filter(Boolean).splice(2, 1).join("");
+}
+
+const currentLocation = extractLocation(window.location.href);
+
+if (currentLocation === "index.html") {
+  localStorage.setItem("activeMenuIdx", 0);
+} else if (currentLocation === "recipes.html") {
+  localStorage.setItem("activeMenuIdx", 2);
+}
+
+menuItems.forEach((menu, index) => {
+  menu.addEventListener("click", () => {
+    localStorage.setItem("activeMenuIdx", index);
+  });
+});
+
+const savedIndex = localStorage.getItem("activeMenuIdx");
+
+if (savedIndex !== null) {
+  menuItems.forEach((menu) => menu.removeAttribute("data-active"));
+  menuItems[savedIndex].setAttribute("data-active", "true");
+}
+
 const resizeObserver = new ResizeObserver(() => {
   document.body.classList.add("resizing");
 
