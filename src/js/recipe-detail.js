3a4289485aa3d15307037e8f6317c51500b684f1
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-08-26 13:44:59 +0900

    Add more recipes to check section recipe-detail page

--- src/js/recipe-detail.js
+++ src/js/recipe-detail.js
@@ -18,6 +18,10 @@ const recipeInstructionsContainer = document.querySelector(
   ".recipe-detail__instructions-list",
 );
 
+const suggestedRecipesContainer = document.querySelector(
+  ".recipe-detail__suggestions-box",
+);
+
 breadcrumbTitle.textContent = recipe.title;
 
 const images = `
@@ -74,3 +78,59 @@ const instructions = recipe.instructions.map((instruction) => {
 });
 
 recipeInstructionsContainer.innerHTML = instructions.join("");
+
+const filteredRecipes = recipesData.filter(
+  (currentRecipe) => currentRecipe.id !== recipe.id,
+);
+
+const randomlyPickedRecipes = new Set();
+
+while (
+  randomlyPickedRecipes.size < 3 &&
+  randomlyPickedRecipes.size < filteredRecipes.length
+) {
+  const randomIdx = Math.floor(Math.random() * filteredRecipes.length);
+  randomlyPickedRecipes.add(filteredRecipes[randomIdx]);
+}
+
+const suggestedRecipes = [...randomlyPickedRecipes];
+
+const moreRecipesToCheck = suggestedRecipes.map((recipe) => {
+  return `<article class="recipe">
+      <picture class="recipe__image">
+          <source
+            srcset=${recipe.image.large}
+            type="image/webp"
+            media="(min-width: 48em)"
+          />
+          <img
+            loading="lazy"
+            src=${recipe.image.small}
+            alt="A Smiling family showing off by holding fresh tomatoes, greens, paprika, and lemon in their modern kitchen"
+          />
+      </picture>
+      <div class="recipe__info">
+          <h3 class="recipe__title">${recipe.title}</h3>
+          <p class="recipe__desc">${recipe.overview}</p>
+      </div>
+      <div class="recipe__preparation">
+        <div>
+         <img src="./img/svgicons/icon-servings.svg" />
+         <p>Servings: ${recipe.servings}</p>
+       </div>
+
+       <div>
+         <img src="./img/svgicons/icon-prep-time.svg" />
+         <p>Prep: ${recipe.prepMinutes}  ${recipe.prepMinutes > 1 ? "mins" : "min"}</p>
+       </div>
+      <div>
+         <img src="./img/svgicons/icon-cook-time.svg" />
+         <p>Cook: ${recipe.cookMinutes} ${recipe.cookMinutes > 1 ? "mins" : "min"} </p>
+       </div>
+       </div>
+      <a href="../recipe-detail.html?id=${recipe.slug}" class="recipe__link button">View Recipe</a>
+    </article>
+  `;
+});
+
+suggestedRecipesContainer.innerHTML = moreRecipesToCheck.join("");
