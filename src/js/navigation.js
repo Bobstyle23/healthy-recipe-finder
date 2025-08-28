9670e63e7d960b2e60dac480632361fa832966eb
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-08-28 14:21:05 +0900

    Update navigation.js to use OOP

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,53 +1,61 @@
-const toggleNavigation = document.querySelector(
-  "[aria-controls='primary-navigation']",
-);
+class Navigation {
+  constructor() {
+    this.toggleNavigation = document.querySelector(
+      "[aria-controls='primary-navigation']",
+    );
+    this.navigationMenu = document.querySelectorAll(".navigation__list li");
+    this.menuItems = [...this.navigationMenu].slice(0, 3);
 
-const navigationMenu = document.querySelectorAll(".navigation__list li");
-
-const menuItems = [...navigationMenu].slice(0, 3);
-
-function extractLocation(location) {
-  return location.split("/").filter(Boolean).splice(2, 1).join("");
-}
-
-const currentLocation = extractLocation(window.location.href);
-
-if (currentLocation === "index.html") {
-  localStorage.setItem("activeMenuIdx", 0);
-} else if (currentLocation === "recipes.html") {
-  localStorage.setItem("activeMenuIdx", 2);
-}
-
-menuItems.forEach((menu, index) => {
-  menu.addEventListener("click", () => {
-    localStorage.setItem("activeMenuIdx", index);
-  });
-});
-
-const savedIndex = localStorage.getItem("activeMenuIdx");
-
-if (savedIndex !== null) {
-  menuItems.forEach((menu) => menu.removeAttribute("data-active"));
-  menuItems[savedIndex].setAttribute("data-active", "true");
-}
-
-const resizeObserver = new ResizeObserver(() => {
-  document.body.classList.add("resizing");
-
-  requestAnimationFrame(() => {
-    document.body.classList.remove("resizing");
-  });
-});
-
-resizeObserver.observe(document.body);
-
-//NOTE: Mobile navigation toggler
-toggleNavigation.addEventListener("click", () => {
-  console.log("clicked");
-  const navigationOpened = toggleNavigation.getAttribute("aria-expanded");
-  if (navigationOpened === "false") {
-    toggleNavigation.setAttribute("aria-expanded", "true");
-  } else {
-    toggleNavigation.setAttribute("aria-expanded", "false");
+    this.#initResizeObserver();
+    this.#initMenuState();
+    this.#initMobileToggle();
   }
-});
+
+  static #extractLocation(location) {
+    return location.split("/").filter(Boolean).splice(2, 1).join("");
+  }
+
+  #initResizeObserver() {
+    this.resizeObserver = new ResizeObserver(() => {
+      document.body.classList.add("resizing");
+      requestAnimationFrame(() => {
+        document.body.classList.remove("resizing");
+      });
+    });
+    this.resizeObserver.observe(document.body);
+  }
+
+  #initMenuState() {
+    const currentLocation = Navigation.#extractLocation(window.location.href);
+
+    if (currentLocation === "index.html") {
+      localStorage.setItem("activeMenuIdx", 0);
+    } else if (currentLocation === "recipes.html") {
+      localStorage.setItem("activeMenuIdx", 2);
+    }
+
+    this.menuItems.forEach((menu, index) => {
+      menu.addEventListener("click", () => {
+        localStorage.setItem("activeMenuIdx", index);
+      });
+    });
+
+    const savedIndex = localStorage.getItem("activeMenuIdx");
+    if (savedIndex !== null) {
+      this.menuItems.forEach((menu) => menu.removeAttribute("data-active"));
+      this.menuItems[savedIndex].setAttribute("data-active", "true");
+    }
+  }
+
+  #initMobileToggle() {
+    this.toggleNavigation.addEventListener("click", () => {
+      const isOpen =
+        this.toggleNavigation.getAttribute("aria-expanded") === "true";
+      this.toggleNavigation.setAttribute("aria-expanded", !isOpen);
+    });
+  }
+}
+
+const navigation = new Navigation();
+
+console.log(navigation);
